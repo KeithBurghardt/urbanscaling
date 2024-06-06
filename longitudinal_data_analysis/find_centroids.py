5ddb3a0342c49828553082492e0072b7b99413d6
Keith Burghardt <keithab@isi.edu>
2024-06-05 18:49:43 -0700

    Add files via upload

--- longitudinal_data_analysis/find_centroids.py
+++ longitudinal_data_analysis/find_centroids.py
@@ -13,13 +13,13 @@ data = data.to_crs(wgs84_proj4string)
 msaid_data = data.groupby('msaid')
 latest_year = 2015
 data = data.loc[data['year']==latest_year,]
-print(data.columns)
-msaids = data['msaid'].drop_duplicates().values
 for msa in [0,1,-1]:
+    print(msa)
+    msaids = data['msaid'].drop_duplicates().values
     if msa>=0:
         msaids = df.loc[df['SA Type']==['uSA','MSA'][msa],'msaid'].drop_duplicates().values
     merged_shp = {msaid:msaid_data.get_group(msaid).unary_union.centroid for msaid in msaids}
-    pk.dump(merged_shp,'msa_centroids_MSA='+str(msa)+'.pkl')
+    pk.dump(merged_shp,open('msa_centroids_MSA='+str(msa)+'.pkl','wb'))
 
 
 
